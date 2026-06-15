9cff7a6ddcd0808a6068c6a03ac65d81e18d2750
Edgar Ricardo Hernandez Fonseca <erhernandez2000@yahoo.com>
2026-06-15 11:49:31 -0500

    first commit

---
+++ src/test/java/rabbitmq_springboot_demo/RabbitmqSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rabbitmq_springboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
